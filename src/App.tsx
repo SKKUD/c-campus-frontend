235bff7c74041f405ab0e2528f987eb65e7338bd
ajs3801 <tt14an@gmail.com>
2023-07-04 23:18:52 +0900

    add components about messageListHeader

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 import FourcutFrame from './components/FourcutFrame/FourcutFrame.components';
 import MessageList from './routes/messageList/messageList.components';
+import MessageListHeader from './routes/messageListHeader/messageListHeader.components';
 
 function App() {
   return (
     <div className="App">
+      <MessageListHeader number="15"/>
       <MessageList />
     </div>
   );

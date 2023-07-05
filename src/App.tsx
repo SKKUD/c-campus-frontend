c0f2a2ba4c3314c33a3ba8d8e8ce40217daf4dee
ajs3801 <tt14an@gmail.com>
2023-07-05 09:47:02 +0900

    implements openMessage view

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,12 @@ import './App.css';
 import FourcutFrame from './components/FourcutFrame/FourcutFrame.components';
 import MessageList from './routes/messageList/messageList.components';
 import MessageListHeader from './routes/messageListHeader/messageListHeader.components';
+import OpenMessage from './routes/openMessage/openMessage.components';
 
 function App() {
   return (
     <div className="App">
-      <MessageListHeader number="15"/>
-      <MessageList />
+      <OpenMessage MessageNumber={2}/>
     </div>
   );
 }

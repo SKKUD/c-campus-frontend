66945fc63b24c233bb6e93a7447220bb1a3b9228
Haeun Oh <73781124+OHaEuN@users.noreply.github.com>
2023-07-12 00:40:41 +0900

    added theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import FourcutFrame from './components/FourcutFrame/FourcutFrame.components';
-import MessageList from './routes/messageList/messageList.components';
-import MessageListHeader from './routes/messageListHeader/messageListHeader.components';
-import OpenMessage from './routes/openMessage/openMessage.components';
-import MessageView from './routes/messageView/messageView.components';
+import React from "react";
+import FourcutFrame from "./components/FourcutFrame/FourcutFrame.components";
+import MessageList from "./routes/messageList/messageList.components";
+import MessageListHeader from "./routes/messageListHeader/messageListHeader.components";
+import OpenMessage from "./routes/openMessage/openMessage.components";
+import MessageView from "./routes/messageView/messageView.components";
 
 function App() {
   return (

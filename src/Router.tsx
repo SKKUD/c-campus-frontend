e8cee520d7bd08eecd24ae6afdc54ed9ff2a74f5
Haeun Oh <73781124+OHaEuN@users.noreply.github.com>
2023-07-12 22:11:35 +0900

    hotfix - router

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/home.components";
 import Main from "./pages/Main/Main.components";
 import MessageFeed from "./pages/MessageFeed/MessageFeed.components";
@@ -13,20 +13,22 @@ import NotFound from "./pages/NotFound/NotFound.components";
 
 const Router = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} /> {/*서비스 소개*/}
-      <Route path="/main" element={<Main />} /> {/*콩머신 화면*/}
-      <Route path="/message/feed" element={<MessageFeed />} /> {/*쪽지 피드*/}
-      <Route path="/message" element={<MessageBox />} />
-      {/*쪽지 보관함(포켓볼)*/}
-      <Route path="/message/:id" element={<MessageView />} />
-      {/*개별 쪽지 조희*/}
-      <Route path="/message/post" element={<PostMessage />} /> {/*쪽지 작성*/}
-      <Route path="/photo" element={<PhotoBox />} /> {/*네컷 보관함*/}
-      <Route path="/photo/:id" element={<PhotoView />} /> {/*개별 네컷 조회*/}
-      <Route path="/photo/post" element={<PostPhoto />} /> {/*콩캠네컷 촬영*/}
-      <Route path="/*" element={<NotFound />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} /> {/*서비스 소개*/}
+        <Route path="/main" element={<Main />} /> {/*콩머신 화면*/}
+        <Route path="/message/feed" element={<MessageFeed />} /> {/*쪽지 피드*/}
+        <Route path="/message" element={<MessageBox />} />
+        {/*쪽지 보관함(포켓볼)*/}
+        <Route path="/message/:id" element={<MessageView />} />
+        {/*개별 쪽지 조희*/}
+        <Route path="/message/post" element={<PostMessage />} /> {/*쪽지 작성*/}
+        <Route path="/photo" element={<PhotoBox />} /> {/*네컷 보관함*/}
+        <Route path="/photo/:id" element={<PhotoView />} /> {/*개별 네컷 조회*/}
+        <Route path="/photo/post" element={<PostPhoto />} /> {/*콩캠네컷 촬영*/}
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 

38381d0f4023480c008527c22042c3cda4e5035f
Haeun Oh <73781124+OHaEuN@users.noreply.github.com>
2023-07-12 21:10:43 +0900

    add header

---
+++ src/components/common/Header.components.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { AppBar, useMediaQuery } from "@mui/material";
+import { WebHeaderContainer } from "./Header.styles";
+import MobileHeaderImg from "../../assets/images/mobileheader.png";
+
+const Header = () => {
+  const match744 = useMediaQuery("(min-width:744px)");
+  return (
+    <>
+      <AppBar>{match744 ? <WebHeader /> : <MobileHeader />}</AppBar>
+    </>
+  );
+};
+
+export default Header;
+
+const MobileHeader = () => {
+  return <img src={MobileHeaderImg} alt="header" />;
+};
+
+const WebHeader = () => {
+  return <WebHeaderContainer></WebHeaderContainer>;
+};
---
+++ src/components/common/Header.styles.tsx
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const WebHeaderContainer = styled.div`
+  width: 100%;
+  height: 60px;
+`;

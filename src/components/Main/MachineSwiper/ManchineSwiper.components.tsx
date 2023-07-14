e98ec9bbfea0348a7f0a4d3074ff3fa6b8cdfb7e
ajs3801 <tt14an@gmail.com>
2023-07-15 08:43:13 +0900

    downgrade swiper to 9.4.1

--- src/components/Main/MachineSwiper/ManchineSwiper.components.tsx
+++ src/components/Main/MachineSwiper/ManchineSwiper.components.tsx
@@ -1,3 +1,5 @@
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+
 import { Swiper, SwiperSlide } from "swiper/react";
 import { SwiperContainer, SwiperSlideCard } from "./ManchineSwiper.styles";
 
@@ -8,15 +10,16 @@ import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 
 const MachineSwiper = () => {
+
   return (
     <SwiperContainer>
       <Swiper
-      // modules={[Navigation, Pagination, Scrollbar, A11y]} << 오류 나네.. 힝구
-      // navigation
-      // pagination={{ clickable: true }}
-      // scrollbar={{ draggable: true }}
-      // onSwiper={(swiper) => console.log(swiper)}
-      // onSlideChange={() => console.log("slide change")}
+        modules={[Navigation, Pagination, Scrollbar, A11y]} // << 오류 나네.. 힝구
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log("slide change")}
       >
         <SwiperSlide>
           <SwiperSlideCard />

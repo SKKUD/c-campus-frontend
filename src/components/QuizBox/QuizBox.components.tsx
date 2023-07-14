1d212e0842e7167dd5931da05a7ceb303ba9a69f
Haeun Oh <73781124+OHaEuN@users.noreply.github.com>
2023-07-14 15:58:49 +0900

    modified styled-components of QuizBox

--- src/components/QuizBox/QuizBox.components.tsx
+++ src/components/QuizBox/QuizBox.components.tsx
@@ -1,22 +1,23 @@
 import { ChangeEvent, useState, useEffect } from "react";
 
 // import styles
-import { 
+import {
   QuizBoxContainer,
   QuizBoxQuitButtonContainer,
   QuizBoxQuitButton,
   QuizBoxTextAreaContainer,
+  QuizBoxTextArea,
   QuizBoxInformation,
   QuizBoxQuizContent,
   QuizBoxCheckButtonContainer,
+  QuizBoxCheckButton,
 } from "./QuizBox.styles";
-
-import "./QuizBox.styles.css";
+import CloseIcon from "@mui/icons-material/Close";
 
 // QuizBox interface
 interface IQuizBox {
-  Quiz: string,
-  Answer: string
+  Quiz: string;
+  Answer: string;
 }
 
 // information 색깔 코드
@@ -25,12 +26,12 @@ const GREEN_COLOR = "#8DC63F";
 const GREY_COLOR = "#808080";
 
 // Quiz와 Answer을 props로 받음
-const QuizBox = ({Quiz, Answer}: IQuizBox) => {
-  const [color, SetColor] = useState<string>(''); // information color 색깔 변수
+const QuizBox = ({ Quiz, Answer }: IQuizBox) => {
+  const [color, SetColor] = useState<string>(""); // information color 색깔 변수
   const [wrongCount, SetWrongCount] = useState<number>(3); // 틀린 횟수 저장하는 변수
   const [isHint, SetIsHint] = useState<boolean>(false); // 힌트가 나오기 시작하는지 저장하는 변수
-  const [information, SetInformation] = useState<string>(''); // information을 저장하는 변수 (몇 번 틀렸는지, 힌트가 뭔지)
-  const [textArea, SetTextArea] = useState<string>(''); // 유저가 정답 적는 곳
+  const [information, SetInformation] = useState<string>(""); // information을 저장하는 변수 (몇 번 틀렸는지, 힌트가 뭔지)
+  const [textArea, SetTextArea] = useState<string>(""); // 유저가 정답 적는 곳
   const [hintCount, SetHintCount] = useState<number>(1); // hint가 얼마나 나왔는지 저장하는 변수, (한 글자씩 공개해야 하므로)
 
   // textArea 바뀔 때, textArea 값을 useState textArea로 넣어줌
@@ -39,7 +40,7 @@ const QuizBox = ({Quiz, Answer}: IQuizBox) => {
 
     // set the searchField value
     SetTextArea(searchFieldString);
-  }
+  };
 
   // hint 만드는 함수 (ex) 수 _ _을 return
   const makeHint = () => {
@@ -50,10 +51,10 @@ const QuizBox = ({Quiz, Answer}: IQuizBox) => {
     // 그렇지 않으면 hintCount만큼 힌트를 만들어 줌
     else {
       // 공개된 힌트
-      var sliceString = Answer.slice(0,hintCount);
+      var sliceString = Answer.slice(0, hintCount);
 
       // 미공개된 힌트로 남은 글자 수 만큼 _로 채워줌
-      for (let i=0; i<(Answer.length - hintCount); i++) {
+      for (let i = 0; i < Answer.length - hintCount; i++) {
         sliceString = sliceString + " _";
       }
 
@@ -73,7 +74,7 @@ const QuizBox = ({Quiz, Answer}: IQuizBox) => {
       // SetWrongCount(3);
       // SetIsHint(false);
       // SetHintCount(1);
-    } 
+    }
 
     // 정답이 아닐 때,
     else {
@@ -81,9 +82,9 @@ const QuizBox = ({Quiz, Answer}: IQuizBox) => {
       if (wrongCount > 1) {
         SetWrongCount(wrongCount - 1);
       }
-      
+
       // 힌트 나오기 시작
-      else if (wrongCount <= 1 ) { 
+      else if (wrongCount <= 1) {
         SetIsHint(true);
         SetColor(GREY_COLOR);
         SetInformation("힌트: " + makeHint());
@@ -98,43 +99,43 @@ const QuizBox = ({Quiz, Answer}: IQuizBox) => {
 
   // 정답이 아닐 때 wrongCount 값이 변하면, information 알림 수정
   useEffect(() => {
-    if (!isHint) { // 힌트가 나오는게 아닐 때, -> 몇 번 틀렸는지 말해주는 것 일 때
+    if (!isHint) {
+      // 힌트가 나오는게 아닐 때, -> 몇 번 틀렸는지 말해주는 것 일 때
       SetColor(RED_COLOR);
-      SetInformation(`틀렸습니다! ${wrongCount}번의 시도 후에 힌트가 공개됩니다.`);
+      SetInformation(
+        `틀렸습니다! ${wrongCount}번의 시도 후에 힌트가 공개됩니다.`
+      );
     }
   }, [wrongCount]);
 
   return (
     <QuizBoxContainer>
       <QuizBoxQuitButtonContainer>
-        <QuizBoxQuitButton>X</QuizBoxQuitButton>
+        <QuizBoxQuitButton>
+          <CloseIcon />
+        </QuizBoxQuitButton>
       </QuizBoxQuitButtonContainer>
 
       <QuizBoxQuizContent>{Quiz}</QuizBoxQuizContent>
 
       <QuizBoxTextAreaContainer>
-        <input 
-          className="QuixBoxAnswer"
+        <QuizBoxTextArea
           type="search"
           placeholder="정답"
           onChange={onAnswerChange}
         />
       </QuizBoxTextAreaContainer>
-      {
-        (wrongCount < 3) ? (<QuizBoxInformation color={color}>{information}</QuizBoxInformation>) : (<div></div>)
-      }
-      
-      <QuizBoxCheckButtonContainer>
-        <button 
-          className="QuizBoxCheckButton"
-          onClick={onClickHandler}
-        >
-          확인
-        </button>
-      </QuizBoxCheckButtonContainer>
-      
+      {wrongCount < 3 ? (
+        <QuizBoxInformation color={color}>{information}</QuizBoxInformation>
+      ) : (
+        ""
+      )}
+
+      <QuizBoxCheckButton variant="contained" onClick={onClickHandler}>
+        확인
+      </QuizBoxCheckButton>
     </QuizBoxContainer>
-  )
+  );
 };
 
 export default QuizBox;
--- src/components/QuizBox/QuizBox.styles.tsx
+++ src/components/QuizBox/QuizBox.styles.tsx
@@ -1,11 +1,12 @@
-import styled from "styled-components";
+import styled from "@emotion/styled";
+import { Button } from "@mui/material";
 
 export const QuizBoxContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   width: 341px;
-  background-color: #FFFFFF;
+  background-color: #ffffff;
   border-radius: 20px;
   margin: auto;
   margin-top: 100px;
@@ -14,16 +15,16 @@ export const QuizBoxContainer = styled.div`
 export const QuizBoxQuitButtonContainer = styled.div`
   display: flex;
   justify-content: right;
-`
+`;
 
 export const QuizBoxQuitButton = styled.button`
-  background-color: #FFFFFF;
+  background-color: #ffffff;
   font-size: 25px;
-  color: black;
+  color: #c8c8c8;
   width: 25px;
   height: 25px;
   border: none;
-  margin: 15px 10px 0px 20px;
+  margin: 15px 20px 0px 20px;
 `;
 
 export const QuizBoxQuizContent = styled.div`
@@ -38,13 +39,38 @@ export const QuizBoxTextAreaContainer = styled.div`
   justify-content: center;
 `;
 
-export const QuizBoxInformation = styled.div<{ color: string}>`
+export const QuizBoxTextArea = styled.input`
+  background-color: #ededed;
+  border: none;
+  border-radius: 10px;
+  outline: none;
+  padding: 10px;
+  width: 296px;
+  height: 48px;
+  line-height: 30px;
+  margin-bottom: 15px;
+`;
+
+export const QuizBoxInformation = styled.div<{ color: string }>`
   display: flex;
   justify-content: center;
-  color: ${props => props.color};
+  color: ${(props) => props.color};
   font-size: 12px;
   margin-bottom: 10px;
-`
+`;
 export const QuizBoxCheckButtonContainer = styled.div`
   display: flex;
+`;
+
+export const QuizBoxCheckButton = styled(Button)`
+  width: 201px;
+  height: 52px;
+  font-size: 30px;
+  font-weight: 500;
+  background-color: #8dc63f;
+  border: none;
+  color: white;
+  border-radius: 28px;
+  margin: auto;
+  margin-bottom: 20px;
 `;

b42c4fe74d9a4bda80e1188bdfc7eb5f10bb19e7
ajs3801 <tt14an@gmail.com>
2023-07-17 09:50:09 +0900

    add modal in messageView

--- src/pages/MessageBox/MessageBox.components.tsx
+++ src/pages/MessageBox/MessageBox.components.tsx
@@ -1,9 +1,7 @@
-import QuizBox from "../../components/QuizBox/QuizBox.components";
 
 const MessageBox = () => {
   return (
     <div>
-      <QuizBox Quiz="우리가 처음 만났던 장소는?" Answer="수선관" />
     </div>
   );
 };

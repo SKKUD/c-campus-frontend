8cce1e0ef86927550c262de5fe7347c47de88a2d
Haeun Oh <73781124+OHaEuN@users.noreply.github.com>
2023-07-26 22:44:33 +0900

    implement photobooth for test

--- src/pages/PostPhoto/PostPhoto.components.tsx
+++ src/pages/PostPhoto/PostPhoto.components.tsx
@@ -1,5 +1,67 @@
+import { useRef, useState, useCallback } from "react";
+import Webcam from "react-webcam";
+
+const videoConstraints = {
+  width: 720,
+  height: 360,
+  facingMode: "user",
+};
+
 const PostPhoto = () => {
-  return <div>PostPhoto</div>;
+  const [isCaptureEnable, setCaptureEnable] = useState<boolean>(false);
+  const webcamRef = useRef<Webcam>(null);
+  const [url, setUrl] = useState<string | null>(null);
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current?.getScreenshot();
+    if (imageSrc) {
+      setUrl(imageSrc);
+    }
+  }, [webcamRef]);
+
+  return (
+    <>
+      <header>
+        <h1>camera app</h1>
+      </header>
+      {isCaptureEnable || (
+        <button onClick={() => setCaptureEnable(true)}>start</button>
+      )}
+      {isCaptureEnable && (
+        <>
+          <div>
+            <button onClick={() => setCaptureEnable(false)}>end </button>
+          </div>
+          <div>
+            <Webcam
+              audio={false}
+              width={540}
+              height={360}
+              ref={webcamRef}
+              screenshotFormat="image/jpeg"
+              videoConstraints={videoConstraints}
+            />
+          </div>
+          <button onClick={capture}>capture</button>
+        </>
+      )}
+      {url && (
+        <>
+          <div>
+            <button
+              onClick={() => {
+                setUrl(null);
+              }}
+            >
+              delete
+            </button>
+          </div>
+          <div>
+            <img src={url} alt="Screenshot" />
+          </div>
+        </>
+      )}
+    </>
+  );
 };
 
 export default PostPhoto;

2db38f53123a03bd2bea63ea28cc81eb5974c5e9
Haeun Oh <73781124+OHaEuN@users.noreply.github.com>
2023-08-07 01:17:11 +0900

    set webmainpage background

---
+++ src/components/WebMainPage/WebMainPage.styles.tsx
@@ -0,0 +1,51 @@
+import styled from "@emotion/styled";
+
+export const WebMainPageContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  background-color: #f5f5f5;
+  padding: 32px 40px;
+`;
+
+export const Ground = styled.div`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 100vw;
+  height: 168px;
+  background-color: #4b5745;
+  z-index: 1;
+`;
+
+export const BoxAbove = styled.div`
+  width: 100%;
+  height: 100vh;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 1300px;
+`;
+
+export const InfoImgConatiner = styled.div`
+  width: 400px;
+  height: 665px;
+  border-radius: 15px;
+  box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.1);
+  z-index: 10;
+`;
+
+export const InfoPaper = styled.img`
+  width: 100%;
+`;
+
+export const ContentConatiner = styled.div`
+  width: 400px;
+  height: 708px;
+  border-radius: 20px;
+  border: 0.5px solid #164300;
+  background: #f7f7f7;
+  box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.1);
+  z-index: 10;
+`;
--- src/pages/WebCongcamMachine/WebCongcamMachine.components.tsx
+++ src/pages/WebCongcamMachine/WebCongcamMachine.components.tsx
@@ -1,5 +1,24 @@
+import {
+  WebMainPageContainer,
+  Ground,
+  BoxAbove,
+  InfoImgConatiner,
+  InfoPaper,
+  ContentConatiner,
+} from "../../components/WebMainPage/WebMainPage.styles";
+import infoDescImg from "../../assets/images/infoDescImg.png";
 const WebCongcamMachine = () => {
-  return <>WebCongcamMachine</>;
+  return (
+    <WebMainPageContainer>
+      <Ground />
+      <BoxAbove>
+        <InfoImgConatiner>
+          <InfoPaper src={infoDescImg} />
+        </InfoImgConatiner>
+        <ContentConatiner></ContentConatiner>
+      </BoxAbove>
+    </WebMainPageContainer>
+  );
 };
 
 export default WebCongcamMachine;

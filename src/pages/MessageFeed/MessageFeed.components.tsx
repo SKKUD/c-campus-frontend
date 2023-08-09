62435f88ce34f66154503aa521e15ed9d0ff6c8c
ajs3801 <tt14an@gmail.com>
2023-08-09 09:51:58 +0900

    change messageFeed data to backend api

--- src/pages/MessageFeed/MessageFeed.components.tsx
+++ src/pages/MessageFeed/MessageFeed.components.tsx
@@ -10,95 +10,127 @@ import {
   OpenMesageEmpty,
 } from "./MessageFeed.styles";
 
-interface IOpenMessage {
-  MessageNumber?: number,
+// interface
+interface IAxiosMessageData {
+  status: number,
+  message: string,
+  data?: IAxiosData[],
 }
 
-interface IOpenMessageData {
-  id: string,
-  color: string,
-  title: string,
-  mainText: string,
-  sender: string,
+interface IAxiosData {
+  messageId: number,
+  userId: number,
+  catergory: string,
+  content: string,
+  author: string,
+  isOpened: boolean,
+  isPulled: boolean,
+  pulledAt: string,
+  imageUuid: string,
+  backgroundColorCode: string,
+  isPublic: boolean,
+  quizContent: string,
+  quizAnswer: string,
+  quizIsSolved: boolean,
+  imageUrl: string,
 }
 
-const MessageFeed = ({MessageNumber}: IOpenMessage) => {
-  const OpenMessageDataSet: IOpenMessageData[] = [
-    // {
-    //   id: "0",
-    //   color: "#D6EABA",
-    //   title: "우리가 먹었던 최고의 학식 메뉴",
-    //   mainText:
-    //     "가는 유소년에게서 것은 두손을 보는 그와 부패뿐이다. 우는 반짝이는 가는 봄바람이다. 그들의 청춘이 가슴이 용기가 사막이다. 그들의 그들은 주는 얼음에 커다란 용기가 밥을 것이다. 주는 같은 때까지 것이다. 우리 창공에 청춘 인생을 붙잡아 곧 원질이 부패뿐이다. 크고 맺어, 사람은 얼마나 그림자는 방지하는 착목한는 예가 약동하다. 있는 희망의 내려온 인생을 피는 따뜻한 뭇 것이다. 따뜻한 두기 영원히 구하지 창공에 예수는 되는",
-    //   sender: "율전짱짱걸",
-    // },
-    // {
-    //   id: "1",
-    //   color: "#B5D589",
-    //   title: "우리가 먹었던 최고의 학식 메뉴",
-    //   mainText:
-    //     "가는 유소년에게서 것은 두손을 보는 그와 부패뿐이다. 우는 반짝이는 가는 봄바람이다.",
-    //   sender: "명륜짱짱걸",
-    // },
-    // {
-    //   id: "2",
-    //   color: "#B0C7AB",
-    //   title: "우리가 먹었던 최고의 학식 메뉴",
-    //   mainText: "가는 유소년에게서",
-    //   sender: "닉네임은최대10글자",
-    // },
-    // {
-    //   id: "3",
-    //   color: "#D6EABA",
-    //   title: "우리가 먹었던 최고의 학식 메뉴",
-    //   mainText:
-    //     "가는 유소년에게서 것은 두손을 보는 그와 부패뿐이다. 우는 반짝이는 가는 봄바람이다. 그들의 청춘이 가슴이 용기가 사막이다. 그들의 그들은 주는 얼음에 커다란 용기가 밥을 것이다. 주는 같은 때까지 것이다. 우리 창공에 청춘 인생을 붙잡아 곧 원질이 부패뿐이다. 크고 맺어, 사람은 얼마나 그림자는 방지하는 착목한는 예가 약동하다. 있는 희망의 내려온 인생을 피는 따뜻한 뭇 것이다. 따뜻한 두기 영원히 구하지 창공에 예수는 되는",
-    //   sender: "율전짱짱걸",
-    // },
-    // {
-    //   id: "4",
-    //   color: "#D6EABA",
-    //   title: "우리가 먹었던 최고의 학식 메뉴",
-    //   mainText:
-    //     "가는 유소년에게서 것은 두손을 보는 그와 부패뿐이다. 우는 반짝이는 가는 봄바람이다. 그들의 청춘이 가슴이 용기가 사막이다. 그들의 그들은 주는 얼음에 커다란 용기가 밥을 것이다. 주는 같은 때까지 것이다. 우리 창공에 청춘 인생을 붙잡아 곧 원질이 부패뿐이다. 크고 맺어, 사람은 얼마나 그림자는 방지하는 착목한는 예가 약동하다. 있는 희망의 내려온 인생을 피는 따뜻한 뭇 것이다. 따뜻한 두기 영원히 구하지 창공에 예수는 되는",
-    //   sender: "율전짱짱걸",
-    // },
-    // {
-    //   id: "5",
-    //   color: "#D6EABA",
-    //   title: "우리가 먹었던 최고의 학식 메뉴",
-    //   mainText:
-    //     "가는 유소년에게서 것은 두손을 보는 그와 부패뿐이다. 우는 반짝이는 가는 봄바람이다. 그들의 청춘이 가슴이 용기가 사막이다. 그들의 그들은 주는 얼음에 커다란 용기가 밥을 것이다. 주는 같은 때까지 것이다. 우리 창공에 청춘 인생을 붙잡아 곧 원질이 부패뿐이다. 크고 맺어, 사람은 얼마나 그림자는 방지하는 착목한는 예가 약동하다. 있는 희망의 내려온 인생을 피는 따뜻한 뭇 것이다. 따뜻한 두기 영원히 구하지 창공에 예수는 되는",
-    //   sender: "율전짱짱걸",
-    // },
-    // {
-    //   id: "6",
-    //   color: "#D6EABA",
-    //   title: "우리가 먹었던 최고의 학식 메뉴",
-    //   mainText:
-    //     "가는 유소년에게서 것은 두손을 보는 그와 부패뿐이다. 우는 반짝이는 가는 봄바람이다. 그들의 청춘이 가슴이 용기가 사막이다. 그들의 그들은 주는 얼음에 커다란 용기가 밥을 것이다. 주는 같은 때까지 것이다. 우리 창공에 청춘 인생을 붙잡아 곧 원질이 부패뿐이다. 크고 맺어, 사람은 얼마나 그림자는 방지하는 착목한는 예가 약동하다. 있는 희망의 내려온 인생을 피는 따뜻한 뭇 것이다. 따뜻한 두기 영원히 구하지 창공에 예수는 되는",
-    //   sender: "율전짱짱걸",
-    // },
-  ];
+// data sample
+const AxiosMessageData: IAxiosMessageData = {
+  status: 200,
+  message: "유저 메시지 전체 조회에 성공했습니다",
+  data: [
+    {
+      messageId: 1,
+      userId: 13,
+      catergory: "우리가 먹었던 최고의 학식 메뉴",
+      content: "하이 헬로 봉주르",
+      author: "명륜 짱짱맨",
+      isOpened: true,
+      isPulled: true,
+      pulledAt: "2023-07-10T16:34:30.388",
+      imageUuid: "2103980192830981209",
+      backgroundColorCode: "#D6EABA",
+      isPublic: true,
+      quizContent: "우리가 처음 만났던 장소는?",
+      quizAnswer: "수선관",
+      quizIsSolved: true,
+      imageUrl: "https://www.skku.edu"
+    },
+    {
+      messageId: 2,
+      userId: 13,
+      catergory: "우리가 먹었던 최고의 학식 메뉴",
+      content: "하이 헬로 봉주르",
+      author: "명륜 짱짱맨",
+      isOpened: true,
+      isPulled: true,
+      pulledAt: "2023-07-10T16:34:30.388",
+      imageUuid: "2103980192830981209",
+      backgroundColorCode: "#B0C7AB",
+      isPublic: true,
+      quizContent: "우리가 처음 만났던 장소는?",
+      quizAnswer: "수선관",
+      quizIsSolved: true,
+      imageUrl: "https://www.skku.edu"
+    },
+    {
+      messageId: 3,
+      userId: 13,
+      catergory: "우리가 먹었던 최고의 학식 메뉴",
+      content: "하이 헬로 봉주르",
+      author: "명륜 짱짱맨",
+      isOpened: true,
+      isPulled: true,
+      pulledAt: "2023-07-10T16:34:30.388",
+      imageUuid: "2103980192830981209",
+      backgroundColorCode: "#D6EABA",
+      isPublic: true,
+      quizContent: "우리가 처음 만났던 장소는?",
+      quizAnswer: "수선관",
+      quizIsSolved: true,
+      imageUrl: "https://www.skku.edu"
+    },
+    {
+      messageId: 4,
+      userId: 13,
+      catergory: "우리가 먹었던 최고의 학식 메뉴",
+      content: "하이 헬로 봉주르",
+      author: "명륜 짱짱맨",
+      isOpened: true,
+      isPulled: true,
+      pulledAt: "2023-07-10T16:34:30.388",
+      imageUuid: "2103980192830981209",
+      backgroundColorCode: "#D6EABA",
+      isPublic: true,
+      quizContent: "우리가 처음 만났던 장소는?",
+      quizAnswer: "수선관",
+      quizIsSolved: true,
+      imageUrl: "https://www.skku.edu"
+    },
+  ]
+}
+
+const MessageFeed = () => {
 
   return (
     <>
       {
-        (OpenMessageDataSet.length !== 0) ? (
+        (AxiosMessageData.data?.length !== 0) ? (
           <MessageFeedContainer>
             <OpenMessageHeader>
-              <OpenMessageHeaderContent>{OpenMessageDataSet.length}개의 공개쪽지</OpenMessageHeaderContent>
+              <OpenMessageHeaderContent>개의 공개쪽지</OpenMessageHeaderContent>
             </OpenMessageHeader>
 
             <OpenMessageContainer>
               {
-                OpenMessageDataSet.map((OpenMessageData) => {
+                AxiosMessageData.data?.map((OpenMessageData) => {
                   return (
-                    <OpenMessageContent key={OpenMessageData.id} color={OpenMessageData.color}>
-                      <OpenMessageContentTitle>{OpenMessageData.title}</OpenMessageContentTitle>
-                      <OpenMessageContentMainText>{OpenMessageData.mainText}</OpenMessageContentMainText>
+                    <OpenMessageContent key={OpenMessageData.messageId} color={OpenMessageData.backgroundColorCode}>
+                      <OpenMessageContentTitle>{OpenMessageData.catergory}</OpenMessageContentTitle>
+                      <OpenMessageContentMainText>{OpenMessageData.content}</OpenMessageContentMainText>
                       <OpenMessageContentSender>
-                        <p className="SenderFrom">From.</p> {OpenMessageData.sender}
+                        <p className="SenderFrom">From.</p> {OpenMessageData.author}
                       </OpenMessageContentSender>
                     </OpenMessageContent>
                   )

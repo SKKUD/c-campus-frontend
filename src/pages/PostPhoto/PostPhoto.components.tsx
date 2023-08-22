7babb75a33afc4147609beaf927ee9c5e17a892f
Haeun Oh <73781124+OHaEuN@users.noreply.github.com>
2023-08-23 08:47:54 +0900

    fix file name

--- src/pages/PostPhoto/PostPhoto.components.tsx
+++ src/pages/PostPhoto/PostPhoto.components.tsx
@@ -12,13 +12,13 @@ import {
   WebGreenBtnWrap,
 } from "./PostPhoto.styles";
 import FramePalette from "../../components/PostPhoto/FramePalette/FramePalette.components";
-import frame1 from "../../assets/images/프레임01.png";
-import frame2 from "../../assets/images/프레임02.png";
-import frame3 from "../../assets/images/프레임03.png";
-import frame4 from "../../assets/images/프레임04.png";
-import frame5 from "../../assets/images/프레임05.png";
-import frame6 from "../../assets/images/프레임06.png";
-import frame7 from "../../assets/images/프레임07.png";
+import frame1 from "../../assets/images/frame01.png";
+import frame2 from "../../assets/images/frame02.png";
+import frame3 from "../../assets/images/frame03.png";
+import frame4 from "../../assets/images/frame04.png";
+import frame5 from "../../assets/images/frame05.png";
+import frame6 from "../../assets/images/frame06.png";
+import frame7 from "../../assets/images/frame07.png";
 import blank from "../../assets/images/blank.png";
 import { getDate } from "../../utils/getDate";
 import IconBtnGroup from "../../components/PostPhoto/IconBtnGroup/IconBtnGroup.components";

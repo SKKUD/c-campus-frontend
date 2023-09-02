3aca925206163afb3243c60da8fb5195b1ab735c
Haeun Oh <73781124+OHaEuN@users.noreply.github.com>
2023-09-02 15:09:05 +0900

    fix postFourcutPhoto - toblob

--- src/hooks/PhotoAxios.ts
+++ src/hooks/PhotoAxios.ts
@@ -31,6 +31,71 @@ export const usePhotoGetApi = () => {
   return [photo];
 };
 
+// 콩캠네컷 찍기로 보낼 때
+// export const usePhotoPostApi = () => {
+//   const currentID = useExtractID();
+
+//   const postFourcutPhoto = async () => {
+//     try {
+//       const el = document.querySelector(".fourcutImage") as HTMLElement;
+
+//       // SVG 문자열을 얻습니다.
+//       const svgString = await htmlToImage.toSvg(el);
+
+//       if (svgString) {
+//         // SVG 문자열을 이미지로 변환하여 Blob으로 얻습니다.
+//         const img = new Image();
+//         img.src = svgString;
+
+//         img.onload = () => {
+//           const canvas = document.createElement("canvas");
+//           canvas.width = img.width;
+//           canvas.height = img.height;
+//           const ctx = canvas.getContext("2d");
+
+//           if (ctx) {
+//             ctx.drawImage(img, 0, 0);
+
+//             canvas.toBlob((pngBlob) => {
+//               if (pngBlob) {
+//                 // Blob을 File 객체로 변환하고 파일명 설정
+//                 const pngFile = new File([pngBlob], "CongcamFourcut.png", {
+//                   type: "image/png",
+//                 });
+
+//                 // pngFile을 사용하여 파일 업로드 또는 저장할 수 있습니다.
+//                 const data = {
+//                   title: "fourcut",
+//                   files: [pngFile],
+//                 };
+
+//                 const formData = new FormData();
+//                 formData.append("file", data.files[0]);
+
+//                 axios
+//                   .post(
+//                     `${process.env.REACT_APP_BACKEND_SERVER}/users/${currentID}/photos`,
+//                     formData
+//                   )
+//                   .then((response) => {
+//                     console.log(response.status);
+//                   })
+//                   .catch((error) => {
+//                     console.log(error);
+//                   });
+//               }
+//             }, "image/png");
+//           }
+//         };
+//       }
+//     } catch (error) {
+//       console.error("오류 발생:", error);
+//     }
+//   };
+
+//   return [postFourcutPhoto];
+// };
+
 export const usePhotoPostApi = () => {
   const currentID = useExtractID();
 
@@ -39,53 +104,34 @@ export const usePhotoPostApi = () => {
       const el = document.querySelector(".fourcutImage") as HTMLElement;
 
       // SVG 문자열을 얻습니다.
-      const svgString = await htmlToImage.toSvg(el);
+      const pngBlob = await htmlToImage.toBlob(el);
 
-      if (svgString) {
-        // SVG 문자열을 이미지로 변환하여 Blob으로 얻습니다.
-        const img = new Image();
-        img.src = svgString;
+      if (pngBlob) {
+        // Blob을 File 객체로 변환하고 파일명 설정
+        const pngFile = new File([pngBlob], "CongcamFourcut.png", {
+          type: "image/png",
+        });
 
-        img.onload = () => {
-          const canvas = document.createElement("canvas");
-          canvas.width = img.width;
-          canvas.height = img.height;
-          const ctx = canvas.getContext("2d");
-
-          if (ctx) {
-            ctx.drawImage(img, 0, 0);
-
-            canvas.toBlob((pngBlob) => {
-              if (pngBlob) {
-                // Blob을 File 객체로 변환하고 파일명 설정
-                const pngFile = new File([pngBlob], "CongcamFourcut.png", {
-                  type: "image/png",
-                });
-
-                // pngFile을 사용하여 파일 업로드 또는 저장할 수 있습니다.
-                const data = {
-                  title: "fourcut",
-                  files: [pngFile],
-                };
-
-                const formData = new FormData();
-                formData.append("file", data.files[0]);
-
-                axios
-                  .post(
-                    `${process.env.REACT_APP_BACKEND_SERVER}/users/${currentID}/photos`,
-                    formData
-                  )
-                  .then((response) => {
-                    console.log(response.status);
-                  })
-                  .catch((error) => {
-                    console.log(error);
-                  });
-              }
-            }, "image/png");
-          }
+        // pngFile을 사용하여 파일 업로드 또는 저장할 수 있습니다.
+        const data = {
+          title: "fourcut",
+          files: [pngFile],
         };
+
+        const formData = new FormData();
+        formData.append("file", data.files[0]);
+
+        axios
+          .post(
+            `${process.env.REACT_APP_BACKEND_SERVER}/users/${currentID}/photos`,
+            formData
+          )
+          .then((response) => {
+            console.log(response.status);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
       }
     } catch (error) {
       console.error("오류 발생:", error);

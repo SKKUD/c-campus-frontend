41d12d82bde02641edd410e2c66656cb0541554e
Haeun Oh <73781124+OHaEuN@users.noreply.github.com>
2023-09-04 18:48:39 +0900

    fix handleShare

--- src/utils/handleShare.ts
+++ src/utils/handleShare.ts
@@ -1,51 +1,34 @@
 import { error } from "console";
 import * as htmlToImage from "html-to-image";
+import { domToBlob } from "modern-screenshot";
 
 export const handleShare = async () => {
   const UA = navigator.userAgent.toLowerCase();
 
-  const pngString = await htmlToImage.toPng(
+  const pngBlob = await domToBlob(
     document.querySelector(".congcamMessage") as HTMLElement
   );
-  if (pngString) {
-    try {
-      const img = new Image();
-      img.src = pngString;
+  if (pngBlob) {
+    setTimeout(() => {
+      try {
+        // Blob을 File 객체로 변환하고 파일명 설정
+        const pngFile = new File([pngBlob], "CongcamMessage.png", {
+          type: "image/png",
+        });
 
-      img.onload = () => {
-        const canvas = document.createElement("canvas");
-        canvas.width = img.width;
-        canvas.height = img.height;
-        const ctx = canvas.getContext("2d");
+        // pngFile을 사용하여 파일 업로드 또는 저장할 수 있습니다.
+        const data = {
+          files: [pngFile],
+        };
 
-        if (ctx) {
-          ctx.drawImage(img, 0, 0);
-
-          canvas.toBlob((pngBlob) => {
-            if (pngBlob) {
-              // Blob을 File 객체로 변환하고 파일명 설정
-              const pngFile = new File([pngBlob], "CongcamMessage.png", {
-                type: "image/png",
-              });
-
-              // pngFile을 사용하여 파일 업로드 또는 저장할 수 있습니다.
-              const data = {
-                files: [pngFile],
-              };
-
-              if (navigator.canShare && navigator.canShare(data)) {
-                navigator.share(data);
-              } else {
-                alert("이미지 공유를 지원하지 않는 브라우저입니다.");
-              }
-            }
-          }, "image/png");
+        if (navigator.canShare && navigator.canShare(data)) {
+          navigator.share(data);
+        } else {
+          alert("이미지 공유를 지원하지 않는 브라우저입니다.");
         }
-      };
-
-     
-    } catch {
-      console.log("error");
-    }
+      } catch {
+        console.log("error");
+      }
+    }, 5000);
   }
 };

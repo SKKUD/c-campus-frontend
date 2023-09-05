57a4da29987422196a4b1b6d5b9b97a72addcb8b
Haeun Oh <73781124+OHaEuN@users.noreply.github.com>
2023-09-05 13:24:02 +0900

    fix detext safari

--- src/pages/SafariAlert/SafariAlert.components.tsx
+++ src/pages/SafariAlert/SafariAlert.components.tsx
@@ -1,10 +1,15 @@
-import { SafariContainer } from "./SafariAlert.styles";
+import { SafariContainer, SafariText } from "./SafariAlert.styles";
 
 const SafariAlert = () => {
-  const UA = navigator.userAgent.toLowerCase();
-  const isChrome = UA.includes("chrome");
-
-  return <SafariContainer>{UA}</SafariContainer>;
+  return (
+    <SafariContainer>
+      <SafariText>
+        콩캠퍼스는 크롬 사용을 권장합니다.
+        <br />
+        크롬으로 접속해주세요 {`:)`}
+      </SafariText>
+    </SafariContainer>
+  );
 };
 
 export default SafariAlert;
